1cb8c46706e9ecb4ef6ecb97cf21b3103c28db51
Owen Busler <owen1050@gmail.com>
2020-12-14 21:36:26 -0500

    added arduino code

---
+++ readCanPrintSerial.ino
@@ -0,0 +1,81 @@
+// demo: CAN-BUS Shield, receive data with check mode
+// send data coming to fast, such as less than 10ms, you can use this way
+// loovee, 2014-6-13
+
+
+#include <SPI.h>
+/*SAMD core*/
+#ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
+    #define SERIAL SerialUSB
+#else
+    #define SERIAL Serial
+#endif
+
+#define CAN_2515
+// #define CAN_2518FD
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+
+// Set SPI CS Pin according to your hardware
+// For Wio Terminal w/ MCP2518FD RPi Hat：
+// Channel 0 SPI_CS Pin: BCM 8
+// Channel 1 SPI_CS Pin: BCM 7
+// Interupt Pin: BCM25
+// *****************************************
+// For Arduino MCP2515 Hat:
+// SPI_CS Pin: D9
+
+#ifdef CAN_2518FD
+#include "mcp2518fd_can.h"
+const int SPI_CS_PIN = 9;
+const int CAN_INT_PIN = 2;
+mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
+#endif
+
+#ifdef CAN_2515
+#include "mcp2515_can.h"
+const int SPI_CS_PIN = 53;
+const int CAN_INT_PIN = 2;
+mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
+#endif                           
+
+void setup() {
+    SERIAL.begin(115200);
+
+#ifdef CAN_2518FD
+    while (0 != CAN.begin((byte)CAN_500K_1M)) {            // init can bus : baudrate = 500k
+#else
+    while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
+#endif
+        //SERIAL.println("CAN BUS Shield init fail");
+        //SERIAL.println(" Init CAN BUS Shield again");
+        delay(100);
+    }
+    //SERIAL.println("CAN BUS Shield init ok!");
+}
+
+
+void loop() {
+    unsigned char len = 0;
+    unsigned char buf[8];
+
+    if (CAN_MSGAVAIL == CAN.checkReceive()) {         // check if data coming
+        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+
+        unsigned long canId = CAN.getCanId();
+
+        SERIAL.print("0x");
+        SERIAL.print(canId, HEX);
+        SERIAL.print("\t");
+        for (int i = 0; i < len; i++) { // print the data
+            SERIAL.print(buf[i], HEX);
+            SERIAL.print("\t");
+        }
+        SERIAL.println();
+    }
+}
+
+/*********************************************************************************************************
+    END FILE
+*********************************************************************************************************/
